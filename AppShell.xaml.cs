de6097025519eaedc55dae7b6fc4adf1d4f853c0
mnbavi <elrubius284@gmail.com>
2026-04-13 14:43:21 -0600

    versión de mi app de notas para móvil y pc

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickNotesApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+namespace QuickNotesApp;
+
+public partial class MainPage : ContentPage
+{
+    // Esta lista guarda las notas en la memoria del cel
+    public ObservableCollection<string> Notas { get; set; } = new ObservableCollection<string>();
+
+    public MainPage()
+    {
+        InitializeComponent();
+        // Conectamos la lista con el diseño
+        ListaNotas.ItemsSource = Notas;
+    }
+
+    private void OnGuardarClicked(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(NotaEditor.Text))
+        {
+            // Agregamos la nota al principio de la lista
+            Notas.Insert(0, $"📌 {NotaEditor.Text}");
+
+            // Limpiamos el editor para la siguiente nota
+            NotaEditor.Text = string.Empty;
+        }
+    }
+}
